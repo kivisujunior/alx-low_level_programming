be8636f77432a59efe31b8157f893080d0ff1843
Joel Kivisu <kivisujunior@gmail.com>
2022-03-15 03:40:13 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,41 +1,19 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
+ * main - check the code
  *
- *  * print_alphabet - check the code for Holberton School students.
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
-
-
+ * Return: Always 0.
+ */
 
 void print_alphabet(void)
-
 {
+	char alp = 'a';
 
-		char alp = 'a';
-
-
-
-			while (alp <= 'z')
-
-					{
-
-								_putchar (alp);
-
-										alp++;
-
-
-
-											}
-
-				_putchar ('\n');
-
-
-
+	while (alp <= 'z')
+	{
+		_putchar (alp);
+		alp++;
+	}
+	_putchar ('\n');
 }
