50757e3373fd3781a0542056c3184051060a6f5e
Joel Kivisu <kivisujunior@gmail.com>
2022-03-16 09:48:43 -0700

    Task 6 edited

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -8,10 +8,16 @@
 
 int _abs(int n)
 {
-
-	if (n < 0)
+	if (n > 0)
 	{
-		return (n * -1);
+		return (n);
 	}
-	return (n);
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
 }
